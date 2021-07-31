ca18f6656060d67a2c1dd456eb763689342fe169
Kaustubh Patil <67751648+kaustubhpatil2611@users.noreply.github.com>
2021-07-31 22:09:49 +0530

    added container_1 simple_loop folder

---
+++ automated_checkpoint_restore/container1/simple_loop/apah.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+chmod +x checkpoint_here.sh
+./simple_loop & ./checkpoint_here.sh
---
+++ automated_checkpoint_restore/container1/simple_loop/checkpoint_here.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "Deleting old checkpoints\n"
+rm -rfv checkpoint/* 2>/dev/null
+echo -e "\n"
+
+PID=$(pidof simple_loop)
+
+
+while [[ $PID -eq $(pidof simple_loop) ]]
+do
+
+sleep 10
+
+#echo "$PID removing already existing files "
+
+# echo "starting dump $PID "
+criu dump -t $PID -D checkpoint/ -j -v4 -R 2>/dev/null
+echo -e "\ncheckpointed current process\n"
+done
+rm -rf checkpoint/* 2>/dev/null
